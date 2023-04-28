8444b21f9daca95cd60c73d8ae6ffaed17b2325e
Olexander Zhurba <starkyzh@gmail.com>
2023-04-28 18:50:33 +0200

    style

--- lesson_031/practice_031/src/TaskVotesCalc.java
+++ lesson_031/practice_031/src/TaskVotesCalc.java
@@ -4,28 +4,29 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class TaskVotesCalc {
-    public static void main(String[] args) throws FileNotFoundException {
-        String fileName = "res/in.txt";
-        HashMap<String, Integer> result = readFromFile(fileName);
-        System.out.println(result);
 
+  public static void main(String[] args) throws FileNotFoundException {
+    String fileName = "res/in.txt";
+    HashMap<String, Integer> result = readFromFile(fileName);
+    System.out.println(result);
+  }
+
+  private static HashMap<String, Integer> readFromFile(String fileName)
+      throws FileNotFoundException {
+    Scanner scanner = new Scanner(new File(fileName));
+    int lines = scanner.nextInt();
+    scanner.nextLine();
+
+    HashMap<String, Integer> result = new HashMap<>();
+    while (lines > 0) {
+      String name = scanner.next();
+      int votes = scanner.nextInt();
+      if (result.containsKey(name)) {
+        votes = votes + result.get(name);
+      }
+      result.put(name, votes);
+      lines--;
     }
-
-    private static HashMap<String, Integer> readFromFile(String fileName) throws FileNotFoundException {
-        Scanner scanner = new Scanner(new File(fileName));
-        int lines = scanner.nextInt();
-        scanner.nextLine();
-
-        HashMap<String, Integer> result = new HashMap<>();
-        while(lines > 0){
-            String name = scanner.next();
-            int votes = scanner.nextInt();
-            if (result.containsKey(name)){
-                votes = votes+result.get(name);
-            }
-            result.put(name,votes);
-            lines--;
-        }
-        return result;
-    }
+    return result;
+  }
 }
